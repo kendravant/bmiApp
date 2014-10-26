86019f9afc74a371a1e6e5c0d93f83d78d4c3383
Kendra Vant <kendra.vant@gmail.com>
2014-10-26 21:50:54 +1100

    Fixed file name

--- ui.R
+++ ui.R
@@ -8,23 +8,28 @@ shinyUI(fluidPage(
   # Sidebar with text input for mass and height
   sidebarLayout(
     sidebarPanel( 
-      p("According to the World Health Organisation, your Body Mass Index or BMI provides a useful, simple classifcation
-        of underweight, overweight and obese weight ranges."),
+      div("According to the World Health Organisation, your Body Mass Index or BMI provides a useful, simple classifcation
+        of underweight, overweight and obese weight ranges.",style = "color:green"),
       br(),
-      p("The BMI, which is calculated from your mass in kg divided by your height in m squared, is independent of age and the same for both genders."),
+      p("The BMI is calculated from your mass in kilograms divided by your height in metres squared."),
+      br(), 
+      p("It is independent of age and the same for both genders.", 
+        span("Calculate yours today!", style = "color:green")),
       br(),
-      numericInput("mass", "How much do you weigh (kg)?", 70, step=1),
-      numericInput("height", "How tall are you (m)?", 1.70, step=0.01),
+      br(),
+      br(),
+      numericInput("mass", "How much do you weigh (in kg)?", 70, step=1),
+      numericInput("height", "How tall are you (in m)?", 1.70, step=0.01),
       actionButton("goButton", "Go calculate!")
     ),
 
     # Report the calculated body mass index to one decimal place
     mainPanel(
-      h3("Your Body Mass Index (BMI) is:"),
+      h4("Your Body Mass Index (BMI) is:"),
       verbatimTextOutput("calculation"),
       textOutput("text1"),
       br(),
-      img(src = "bmipic.png", height = 420, width = 680)
+      img(src = "BMIpic.png", height = 420, width = 680)
     )
   )
 ))
